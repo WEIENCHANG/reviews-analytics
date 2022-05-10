c25d5d9f4c01879edc669410c1ada5020c32e88a
WEIENCHANG <104292609+WEIENCHANG@users.noreply.github.com>
2022-05-10 23:26:58 +0800

    Add files via upload

--- read.py
+++ read.py
@@ -11,8 +11,22 @@ print('檔案讀取完了, 總共有', len(data), '筆資料')
 sum_len = 0
 for d in data:
 	sum_len += len(d)
-
 print('平均是', sum_len/len(data))
+
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0])
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '筆留言提到good')
+print(good[0])
 
 
 print(data[0])
